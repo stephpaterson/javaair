a4ec437b5c3342c209726575b0fb37240474e166
Steph Paterson <steph.c.paterson@gmail.com>
2022-04-02 17:10:33 +0100

    created PlaneType enum

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,14 @@
 public enum PlaneType {
+    AIRBUS_A350(100, 100),
+    BOEING747(2, 150);
+
+    private final int capacity;
+    private final int weight;
+
+    PlaneType(int capacity, int weight) {
+        this.capacity = capacity;
+        this.weight = weight;
+    }
 }
+
+
